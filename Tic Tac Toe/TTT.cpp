8cb3523286652b86b8bedd143933a44588079a1c
Aimanislam <57367836+Aimanislam@users.noreply.github.com>
2021-11-13 01:55:48 +0500

    Design the project

---
+++ Tic Tac Toe/TTT.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+char number [10] = {'0', '1','2','3','4','5','6','7','8','9',};
+void board();
+int main()
+ {
+    }
+
+void board()
+{
+cout << " ====================== " <<endl;
+cout << "     Tic Tac Toe    "<<endl;
+cout << " ====================== " <<endl;
+cout << " Player 1 (X) & Player 2 (O) "  <<endl <<endl;
+cout << "  |    |  " <<endl;
+cout << "  " << number[1] << "  |  " << number[2] << "  |  " << number[1] <<endl;
+
+
+
+
+
+
+
+
+}
